ea24fe11de532615dee8c9dd709b5a4d07f42013
Jozef Zitt <jozefzitt@Jozefs-MacBook-Air.local>
2023-10-17 08:31:00 +0200

    upload

--- src/ConsumptionInput/ConsumptionInput.js
+++ src/ConsumptionInput/ConsumptionInput.js
@@ -4,7 +4,7 @@ import './consumptionInput.css'
 export default function ConsumptionInput(props) {
   return (
   <div className="App">
-      <h3>Fuel consumption</h3>
+      <h2 id='fuel-consumption-title'>Fuel consumption</h2>
       Current KM <input id='kmInput' type='number' onChange={props.update}></input>
       Paid for fuel <input id='moneyInput' type='number' onChange={props.update}></input>
       Refueled liters <input id='fuelInput' type='number' onChange={props.update}></input>
